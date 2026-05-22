3e7669c2d9ddf83ecf03b13d8dcb2dac401eb38a
codewithavra <244584119+codewithavra@users.noreply.github.com>
2026-05-22 05:51:05 +0530

    Update backend dependencies to include LangChain community and textsplitters, enhance PDF handling in worker, and adjust TypeScript module settings for improved compatibility.

--- backend/src/lib/worker.ts
+++ backend/src/lib/worker.ts
@@ -2,7 +2,8 @@
  * Node modules
  */
 import { Worker } from 'bullmq';
-
+import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf"
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
 const worker  = new Worker('file-upload-queue',
   async (job)=> {
     const data = JSON.parse(job.data)
@@ -14,6 +15,26 @@ const worker  = new Worker('file-upload-queue',
      * call the openai embedding model for every chunk
      * store the chunk in quadrant db
      */
+
+    /**
+     * Load the pdf
+     */
+
+    const loader = new PDFLoader(data.path)
+
+    /**
+     * Create the document
+     */
+    const docs = await loader.load()
+    console.log(docs)
+
+    /**
+     * Text Splitting
+     */
+
+    // const splitter = new RecursiveCharacterTextSplitter({ chunkSize: 100, chunkOverlap: 0 })
+    // const texts = splitter.splitText(docs)
+
   },{
     concurrency : 100,
     connection : {
