1f078baca9e3a7d77e3ee8dad24b7d428d004b70
codewithavra <244584119+codewithavra@users.noreply.github.com>
2026-06-13 19:16:48 +0530

    update loader

--- frontend/src/components/Loader.tsx
+++ frontend/src/components/Loader.tsx
@@ -1,8 +1,7 @@
 const Loader = () => {
   return (
-    <div className="flex h-svh w-screen flex-col items-center justify-center bg-background text-2xl text-foreground">
-      <p>Loading....</p>
-      <p>Please wait for a moment.</p>
+    <div className="bg-background h-svh w-screen flex justify-center items-center">
+        <img src="/icon.svg" alt="icon" />
     </div>
   )
 }
