85ff6806e8c965e5dbfb90e860eeff7e9535e26e
codewithavra <244584119+codewithavra@users.noreply.github.com>
2026-06-15 02:56:49 +0530

    feat: add multer for file uploads, create queue and worker modules, and update package.json scripts

---
+++ backend/src/lib/multer.ts
@@ -0,0 +1,29 @@
+/**
+ * Node Imports
+ */
+import multer from "multer";
+import path from "path";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, uniqueSuffix + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 50 * 1024 * 1024,
+  },
+  fileFilter: (_req, file, cb) => {
+    if (file.mimetype === "application/pdf") {
+      cb(null, true);
+    } else {
+      cb(new Error("Only PDF files are allowed"));
+    }
+  },
+});
