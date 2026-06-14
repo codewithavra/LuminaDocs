cb8b4b95e2dab070f6a32a512086ff23cd76655f
codewithavra <244584119+codewithavra@users.noreply.github.com>
2026-06-14 15:29:25 +0530

    fix: remove redundant environment variables from config

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -7,6 +7,8 @@ export const env = {
     PORT : process.env.PORT || 8000,
     CORS_ORIGIN : process.env.CORS_ORIGIN,
     NODE_ENV : process.env.NODE_ENV,
-    OPENAI_SECRET_KEY : process.env.OPENAI_SECRET_KEY,
-    QDRANT_URL : process.env.QDRANT_URL
+    GOOGLE_API_KEY : process.env.GOOGLE_API_KEY,
+    POSTGRESQL_URI : process.env.POSTGRESQL_URI,
+    UPSTASH_REDIS_REST_URL : process.env.UPSTASH_REDIS_REST_URL,
+    UPSTASH_REDIS_REST_TOKEN : process.env.UPSTASH_REDIS_REST_TOKEN
 }
