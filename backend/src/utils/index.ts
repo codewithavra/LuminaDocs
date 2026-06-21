992bd24abb32814520a19271eb56af99b36da8d1
codewithavra <244584119+codewithavra@users.noreply.github.com>
2026-06-21 20:06:16 +0530

    feat: implement ApiError and ApiResponse classes, add asyncHandler utility, and update index exports

---
+++ backend/src/utils/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Other Imports
+ */
+
+import { asyncHandler } from "./asyncHandler";
+import { ApiResponse } from "./ApiResponse";
+import { ApiError } from "./ApiError";
+
+export {
+    asyncHandler,
+    ApiResponse,
+    ApiError
+}
