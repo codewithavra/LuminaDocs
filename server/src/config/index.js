e0b9759a72c991bde5babd8565bbfdb63a32d540
codewithavra <244584119+codewithavra@users.noreply.github.com>
2026-08-11 08:19:38 +0530

    feat: update index.js to export llm and ioRedisConnection, removing groq

--- server/src/config/index.js
+++ server/src/config/index.js
@@ -1,11 +1,12 @@
 import { env } from "./env.js";
 import { embeddings } from "./gemini.js";
 import { vectorStore } from "./pinecone.js";
-import { groq } from "./groq.js";
-
+import { llm } from "./groq.js";
+import { ioRedisConnection } from "./redis.js";
 export {
     env,
     embeddings,
     vectorStore,
-    groq
+    llm,
+    ioRedisConnection
 }
