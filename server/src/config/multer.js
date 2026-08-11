a3a462602c2e4cc4997eaf1b0d691b3b5d165bfa
codewithavra <244584119+codewithavra@users.noreply.github.com>
2026-08-11 08:19:43 +0530

    feat: add multer configuration for file uploads with PDF validation

---
+++ server/src/config/multer.js
@@ -0,0 +1,33 @@
+import multer from "multer";
+import path from "node:path";
+
+const uploadDir = path.resolve(process.cwd(), "uploads");
+
+const storage = multer.diskStorage({
+  destination: uploadDir,
+  filename: function (req, file, cb) {
+    const unique = `${Date.now()}-${file.originalname}`;
+    cb(null, unique);
+  },
+});
+
+export const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 20 * 1024 * 1024,
+  },
+  fileFilter: (_req, file, cb) => {
+    const allowed = [
+      "application/pdf",
+    //   "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+    //   "text/plain",
+    ];
+
+    if (!allowed.includes(file.mimetype)) {
+      cb(new Error("❌Only PDFs are allowed"));
+      return;
+    }
+
+    cb(null, true);
+  },
+});
