dabdb91615f158fe962a313ea47e69eedf9f743f
codewithavra <244584119+codewithavra@users.noreply.github.com>
2026-08-11 08:19:20 +0530

    feat: refactor app.js to improve express app configuration and CORS setup

--- server/src/app.js
+++ server/src/app.js
@@ -1,3 +1,21 @@
 import express from "express"
+import cors from "cors";
 
-export const app = express()
+export const app = express();
+
+app.set("trust proxy", 1);
+
+app.use(
+  cors({
+    credentials: true,
+    origin: env.CORS_ORIGIN,
+  }),
+);
+
+app.use(e.json({ limit: "10kb" }));
+app.use(
+  e.urlencoded({
+    extended: true,
+    limit: "10kb",
+  }),
+);
